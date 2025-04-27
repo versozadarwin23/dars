30a0193b9ae7d57a32e6796da50b0694f56e0b24
versozadarwin23 <109713017+versozadarwin23@users.noreply.github.com>
2025-04-27 22:37:10 +0800

    Update dars.py

--- dars.py
+++ dars.py
@@ -438,7 +438,7 @@ def create_fbunconfirmed(account_type, usern, gender):
 def NEMAIN():
     """Handles new registration method automatically."""
 
-    max_create = 10  # Set how many accounts to generate
+    max_create = 50  # Set how many accounts to generate
     account_type = 1  # 1 = Philippines
     gender = 1  # 1 = Male, 2 = Female
     # --------------------------------------
